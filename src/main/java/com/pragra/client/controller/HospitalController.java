3b5e8c8800ec38c531d1801c94408b52f5701ce6
Ravneet Sohal <ravneetsohal@Ravneets-MacBook-Air.local>
2024-06-27 03:31:57 -0400

    Added POST consume request

--- src/main/java/com/pragra/client/controller/HospitalController.java
+++ src/main/java/com/pragra/client/controller/HospitalController.java
@@ -2,11 +2,11 @@ package com.pragra.client.controller;
 
 import com.pragra.client.entity.Hospital;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,5 +25,15 @@ public class HospitalController {
                 .bodyToMono(Hospital[].class)
                 .block();
         return Arrays.asList(hospitalData);
+    }
+
+    @PostMapping("/add")
+    public Hospital addNurse(@RequestBody Hospital hospital){
+        return webClient.post()
+                .uri("http://localhost:8080/hospital/add")
+                .body(Mono.just(hospital), Hospital.class)
+                .retrieve()
+                .bodyToMono(Hospital.class)
+                .block();
     }
 }
